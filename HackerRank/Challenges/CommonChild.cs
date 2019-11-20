201764b1c2bce81a137b1ead8bad5d01ce8e4db8
Glenn Venzke <glennvenzke@comcast.net>
2019-11-20 14:51:44 -0600

    1st refactor - Commonchild

--- HackerRank/Challenges/CommonChild.cs
+++ HackerRank/Challenges/CommonChild.cs
@@ -24,7 +24,8 @@ namespace HackerRank
             Assert.AreEqual(3, commonChild("ABCD", "ABDC"), "ABCD && ABDC");
             Assert.AreEqual(2, commonChild("ABCDEF", "FBDAMN"), "ABCDEF && FBDAMN");
             Assert.AreEqual(3, commonChild("SHINCHAN", "NOHARAAA"), "SHINCHAN && NOHARAAA");
-            //Assert.AreEqual(15, commonChild("WEWOUCUIDG CGTRMEZEPX ZFEJWISRSB BSYXAYDFEJ JDLEBVHHKS", "FDAGCXGKCT KWNECHMRXZ WMLRYUCOCZ HJRRJBOAJO QJZZVUYXIC"), "WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHK && FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC");
+            Assert.AreEqual(3, commonChild("FDAGCXGKCT", "CGTRMEZEPX"), "FDAGCXGKCT && CGTRMEZEPX");
+            //Assert.AreEqual(15, commonChild("WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS", "FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC"), "WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHK && FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC");
         }
 
         public override object RunManualTest()
@@ -42,41 +43,90 @@ namespace HackerRank
             }
             var sb = new StringBuilder();
             var matches = new List<int>();
-            int innerStartIndex = 0;
-            int outerStartIndex = 0;
-            bool matchFound = false;
-            while (outerStartIndex < s1.Length)
+            var lhs = new HashSet<Tuple<char, int>>();
+            var rhs = new HashSet<Tuple<char, int>>();
+            for (var x = 0; x < s1.Length; x++)
             {
-                for (var n = outerStartIndex; n < s1.Length; n++)
+                var c1 = s1[x];
+                for (var y = 0; y < s2.Length; y ++)
                 {
-                    var c1 = s1[n]; 
-                    for (var i = innerStartIndex; i < s2.Length; i++)
+                    var c2 = s2[y];
+                    if (c1 == c2)
                     {
-                        var c2 = s2[i];
-                        if (c2 == c1)
-                        {
-                            matchFound = true;
-                            sb.Append(c1);
-                            innerStartIndex = i + 1;
-                            break;
-                        }
+                        lhs.Add(new Tuple<char, int>(c1, x));
+                        rhs.Add(new Tuple<char, int>(c2, y));
                     }
                 }
+            }
+            if (lhs.Count == 0 || rhs.Count == 0)
+            {
+                return 0;
+            }
 
+            Tuple<char, int>[] primary;
+            Tuple<char, int>[] secondary;
+            if (lhs.Count > rhs.Count)
+            {
+                primary = lhs.OrderBy(o => o.Item2).ToArray();
+                secondary = rhs.OrderBy(o => o.Item2).ToArray();
+            } else
+            {
+                primary = rhs.OrderBy(o => o.Item2).ToArray();
+                secondary = lhs.OrderBy(o => o.Item2).ToArray();
+            }
+            
+            //var reset = false;
+            //Start:
+            foreach (var primaryItem in primary)
+            {
+                char charToTarget = primaryItem.Item1;
+                var secondaryItem = secondary.First(o => o.Item1 == primaryItem.Item1);
+                int curMatchThreshold = -1;
+                int startIndex = -1;
+ 
+                startIndex = Array.IndexOf(primary, new Tuple<char, int>(charToTarget, primaryItem.Item2));
 
-
+                for (var i = startIndex; i < primary.Length; i++)
+                {
+                    var item = primary[i];
+                    var match = secondary.FirstOrDefault(o => o.Item1 == item.Item1 && o.Item2 > curMatchThreshold);
+                    if (match != null)
+                    {
+                        sb.Append(match.Item1);
+                        curMatchThreshold = match.Item2;
+                    }
+                }
                 if (sb.Length > 0)
                 {
                     matches.Add(sb.Length);
-                    sb.Length = 0;                    
+                    sb.Length = 0;
                 }
-                outerStartIndex++;
-                innerStartIndex = 0;
-            }
-           // var recursematchCount = recurse ? commonChild(s2, s1, false) : 0;
-            var matchCount = matches.Count == 0 ? 0 : matches.Max(o => o);
+                startIndex = Array.IndexOf(secondary, new Tuple<char, int>(charToTarget, secondaryItem.Item2));
 
-            return matchCount;
+                for (var i = startIndex; i < secondary.Length; i++)
+                {
+                    var item = secondary[i];
+                    var match = primary.FirstOrDefault(o => o.Item1 == item.Item1 && o.Item2 > curMatchThreshold);
+                    if (match != null)
+                    {
+                        sb.Append(match.Item1);
+                        curMatchThreshold = match.Item2;
+                    }
+                }
+                if (sb.Length > 0)
+                {
+                    matches.Add(sb.Length);
+                    sb.Length = 0;
+                }
+                //if (!reset)
+                //{
+                //    reset = true;
+
+                //}
+
+            }
+
+            return matches.Count == 0 ? 0 : matches.Max(o => o);
         }
     }
 }
